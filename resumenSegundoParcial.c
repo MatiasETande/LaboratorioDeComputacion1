9cb3948f1601f45713e04b3e5ec79218b3aa17fa
Matias Emanuel Tande <matiasetande@gmail.com>
2022-05-25 13:28:46 -0300

    se agrega ejemplo de declaración de String

--- resumenSegundoParcial.c
+++ resumenSegundoParcial.c
@@ -15,6 +15,22 @@ Resumen:
 int main(){	
 
 	
+	
+	//Declaracion de una Cadena o String
+	/*
+	siempre se debe tener en cuenta un caracter adicional a la cadena mas larga
+	que se vaya a guandar, esto para guardar en la terminacion de la cadena un 
+	nulo el cual se especifica con '\0'
+	En el siguiente ejemplo la cadena quedaria compuesta de la siguiente forma:
+	
+	char cadena[5] = {'h','o','l','a','\0'} 
+	
+	(Las dos formas de inicializar la cadena son equivalentes)	
+	*/
+	char unString[5] = "hola";
+	printf ("%s",unString);
+	
+	
 
 	
 	
