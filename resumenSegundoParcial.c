590aba8f0f27f8438dba37bbec1b53c75775744b
Matias Emanuel Tande <matiasetande@gmail.com>
2022-05-25 12:01:32 -0300

    Se crea el archivo resumenSegundoParcial

---
+++ resumenSegundoParcial.c
@@ -0,0 +1,25 @@
+/*
+Tande, Matias Emanuel.
+Resumen:
+-Funciones y metodos
+-Cadenas
+-Arreglos y Matrices
+*/
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+
+int main(){	
+
+	
+
+	
+	
+	
+	
+	
+	
+}
