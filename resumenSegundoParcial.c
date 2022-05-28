cea4bf0f23bf5d025e849a8282d7e96338776cd5
Matias Emanuel Tande <matiasetande@gmail.com>
2022-05-28 15:22:01 -0300

    ejemplos de la libreria string.h

--- resumenSegundoParcial.c
+++ resumenSegundoParcial.c
@@ -9,6 +9,7 @@ Resumen:
 
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>//libreria ultizada para el menejo de string
 
 
 
@@ -19,7 +20,7 @@ int main(){
 	//Declaracion de una Cadena o String
 	/*
 	siempre se debe tener en cuenta un caracter adicional a la cadena mas larga
-	que se vaya a guandar, esto para guardar en la terminacion de la cadena un 
+	que se vaya a guardar, esto para guardar en la terminacion de la cadena un 
 	nulo el cual se especifica con '\0'
 	En el siguiente ejemplo la cadena quedaria compuesta de la siguiente forma:
 	
@@ -28,12 +29,43 @@ int main(){
 	(Las dos formas de inicializar la cadena son equivalentes)	
 	*/
 	char unString[5] = "hola";
-	printf ("%s",unString);
+	char unaCadena[] = {' ','m','u','n','d','o','\0'};
+	printf ("%s\n",unString);
+	
+	int i = strcmp(unString,"hola");
+	if (i==0){
+		printf("Son iguales");
+	}else{
+		printf("No son iguales");
+	}
 	
 	
-
+	// la funcion strlen(cadena); retorna la longitud de la cadena
+	
+	printf("\nLongitud del string: %d\n",strlen(unString));
+	for (int i=0 ; i<4 ; i++){
+		printf("%c\n",unString[i]);
+		
+	}
 	
 	
+	printf("\nLongitud de la cadena: %d\n",strlen(unaCadena));
+	for (int i=0 ; i<6 ; i++){
+		printf("%c\n",unaCadena[i]);
+	}
+	
+	//la funcion strcat(cadena1, cadena2); agrega una cadena a otra
+	strcat(unString,unaCadena);
+	printf("\n%s\n",unString);
+	
+	/*i = strcmp(unString,"hola mun");
+	if (i==0){
+		printf("Son iguales");
+	}else{
+		printf("No son iguales");
+	}*/
+	
+	printf("\n%s\n",unString);
 	
 	
 	getch();
